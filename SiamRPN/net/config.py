ffdeba68f50aa0c2309d095e42bf0eeac680ff0e
SeanCheng1019 <386822664@qq.com>
2019-10-29 14:38:09 +0800

    del

--- SiamRPN/net/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    anchor_num = 5
-    instance_size = 271
-    exemplar_size = 127
-    instance_crop_size = 511
-    context_margin_amount = 0.5
-    total_stride = 8
--- SiamRPN/test_demo.py
+++
@@ -1,9 +0,0 @@
-from net.net_siamrpn import SiameseAlexNet
-import numpy as np
-import torch as t
-net = SiameseAlexNet()
-input_template = t.from_numpy(np.random.random(size=(1, 3, 127, 127)) * 255).float()
-input_detection = t.from_numpy(np.random.random(size=(1, 3, 271, 271)) * 255).float()
-
-cls_score, reg_score = net(input_template, input_detection)
-print(cls_score, reg_score)
